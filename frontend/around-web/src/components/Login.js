17f2cc2962735d31730c073ea9f619c8f164ffeb
Shaoshuai-Xu <xu.shaosh@northeastern.edu>
2021-10-24 16:10:05 -0700

    [Add] import react router, build basic framework

---
+++ frontend/around-web/src/components/Login.js
@@ -0,0 +1,87 @@
+import React from "react";
+import {Form, Input, Button, message} from "antd";
+import {UserOutlined, LockOutlined, AntCloudOutlined} from "@ant-design/icons";
+import {Link} from "react-router-dom";
+import axios from "axios";
+
+import {BASE_URL} from "../constants";
+
+function Login(props) {
+    const {handleLoggedIn} = props;
+
+    const onFinish = (values) => {
+        const {username, password} = values;
+        const opt = {
+            method: "POST",
+            url: `${BASE_URL}/signin`,
+            data: {
+                username: username,
+                password: password
+            },
+            headers: {"Content-Type": "application/json"}
+        };
+        // send user info to server
+        axios(opt)
+            // case1: success: send token data to app
+            .then((res) => {
+                if (res.status === 200) {
+                    const {data} = res;
+                    handleLoggedIn(data);
+                    message.success("Login succeed! ");
+                }
+            })
+            // case2: failed -> display error info
+            .catch((err) => {
+                console.log("login failed: ", err.message);
+                message.error("Login failed!");
+            });
+    };
+
+    return (
+        <Form name="normal_login" className="login-form" onFinish={onFinish}>
+            <div className="login-icon">
+                <AntCloudOutlined/>
+            </div>
+            <Form.Item
+                name="username"
+                rules={[
+                    {
+                        required: true,
+                        message: "Please input your Username!"
+                    }
+                ]}
+            >
+                <Input
+                    prefix={<UserOutlined className="site-form-item-icon"/>}
+                    placeholder="Username"
+                    size={"large"}
+                />
+            </Form.Item>
+            <Form.Item
+                name="password"
+                rules={[
+                    {
+                        required: true,
+                        message: "Please input your Password!"
+                    }
+                ]}
+            >
+                <Input.Password
+                    prefix={<LockOutlined className="site-form-item-icon"/>}
+                    type="password"
+                    placeholder="Password"
+                    size={"large"}
+                />
+            </Form.Item>
+
+            <Form.Item>
+                <Button type="primary" htmlType="submit" className="login-form-button">
+                    Log in
+                </Button>
+                Or <Link to="/register">register now!</Link>
+            </Form.Item>
+        </Form>
+    );
+}
+
+export default Login;
---
+++ frontend/around-web/src/components/TopBar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import logo from "../assets/images/logo.svg";
+import "../styles/TopBar.css";
+
+import {LogoutOutlined} from '@ant-design/icons';
+
+function TopBar(props) {
+    const {isLoggedIn, handleLogout} = props;
+    return (
+        <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo"/>
+            <span className="App-title">Around Web</span>
+            {
+                isLoggedIn ?
+                    <LogoutOutlined className='logout' onClick={handleLogout}/>
+                    :
+                    null
+            }
+        </header>
+    );
+}
+
+export default TopBar;
+
