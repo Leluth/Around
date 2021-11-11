2ae7f5d44ced71464512c77d00ef8290bb98a269
Shaoshuai-Xu <xu.shaosh@northeastern.edu>
2021-11-10 18:44:47 -0800

    [Add] import react grid gallery, build main page

--- frontend/around-web/src/components/Home.js
+++ frontend/around-web/src/components/Home.js
@@ -1,7 +1,116 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Tabs, message, Row, Col, Button } from "antd";
+import axios from "axios";
+
+import SearchBar from "./SearchBar";
+import PhotoGallery from "./PhotoGallery";
+import { SEARCH_KEY, BASE_URL, TOKEN_KEY } from "../constants";
+
+const { TabPane } = Tabs;
 
 function Home(props) {
-    return <div>Home</div>;
+    const [posts, setPost] = useState([]);
+    const [activeTab, setActiveTab] = useState("image");
+    const [searchOption, setSearchOption] = useState({
+        type: SEARCH_KEY.all,
+        keyword: ""
+    });
+
+    useEffect(() => {
+        const { type, keyword } = searchOption;
+        fetchPost(searchOption);
+    }, [searchOption]);
+
+    const fetchPost = (option) => {
+        const { type, keyword } = option;
+        let url = "";
+
+        if (type === SEARCH_KEY.all) {
+            url = `${BASE_URL}/search`;
+        } else if (type === SEARCH_KEY.user) {
+            url = `${BASE_URL}/search?user=${keyword}`;
+        } else {
+            url = `${BASE_URL}/search?keywords=${keyword}`;
+        }
+
+        const opt = {
+            method: "GET",
+            url: url,
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem(TOKEN_KEY)}`
+            }
+        };
+
+        axios(opt)
+            .then((res) => {
+                if (res.status === 200) {
+                    setPost(res.data);
+                }
+            })
+            .catch((err) => {
+                message.error("Fetch posts failed!");
+                console.log("fetch posts failed: ", err.message);
+            });
+    };
+
+    const renderPosts = (type) => {
+        if (!posts || posts.length === 0) {
+            return <div>No data!</div>;
+        }
+        if (type === "image") {
+            const imageArr = posts
+                .filter((item) => item.type === "image")
+                .map((image) => {
+                    return {
+                        src: image.url,
+                        user: image.user,
+                        caption: image.message,
+                        thumbnail: image.url,
+                        thumbnailWidth: 300,
+                        thumbnailHeight: 200
+                    };
+                });
+
+            return <PhotoGallery images={imageArr} />;
+        } else if (type === "video") {
+            return (
+                <Row gutter={32}>
+                    {posts
+                        .filter((post) => post.type === "video")
+                        .map((post) => (
+                            <Col span={8} key={post.url}>
+                                <video src={post.url} controls={true} className="video-block" />
+                                <p>
+                                    {post.user}: {post.message}
+                                </p>
+                            </Col>
+                        ))}
+                </Row>
+            );
+        }
+    };
+
+    const operations = <Button>Upload</Button>;
+    return (
+        <div className="home">
+            <SearchBar />
+            <div className="display">
+                <Tabs
+                    onChange={(key) => setActiveTab(key)}
+                    defaultActiveKey="image"
+                    activeKey={activeTab}
+                    tabBarExtraContent={operations}
+                >
+                    <TabPane tab="Images" key="image">
+                        {renderPosts("image")}
+                    </TabPane>
+                    <TabPane tab="Videos" key="video">
+                        {renderPosts("video")}
+                    </TabPane>
+                </Tabs>
+            </div>
+        </div>
+    );
 }
 
 export default Home;
